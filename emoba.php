d938cb226c3e3a24239f9f1a2bde1de8494d7df8
kirkpatrick <kirkpatrick@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-20 18:32:37 +0000

    tag V1.2  not yet stable version
    git-svn-id: https://plugins.svn.wordpress.org/emoba-email-obfuscator-advanced/tags/1.2@175640 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- emoba.php
+++ emoba.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: emObA - Email Obfuscator Advanced
 Description: Scans pages, posts, comments for email addresses and creates mailto links which are difficult for 'bot harvesters to find. Typing A@B.C results in a "A-B-C" link;  href="mailto:" links are preserved but obfuscated; the special occurrence "[Name] A@B.C"  is recognized and results in a link on "Name".  Without JavaScript, hovering  pops up the email with graphic glyphs for "@" and ".".  (Based on eMob Email Obfuscator 1.1 by Billy Halsey.)
-Version: 1.1
+Version: 1.2
 License: GPL
 Author: Kim Kirkpatrick
-Author URI:
+Author URI: http://kirknet/wpplugins
 */
 /*  Copyright 2009  Kim Kirkpatrick  (email : kirk@kirknet.org)
 
@@ -23,6 +23,16 @@ Author URI:
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
+
+
+/****
+Option (at present, the only one):
+If CLICKPOP is true, hovering over the link "addr" changes it to "Click to email addr". (This switch has no effect if JavaScript is off.)
+****/
+define ("CLICKPOP", false);
+
+
+
 /****
 Place the css in the head:
 ****/
@@ -30,13 +40,6 @@ function emoba_includes(){
   wp_enqueue_style( 'emoba_style', plugin_dir_url(__FILE__) . 'emoba_style.css');
 }
 add_action('init','emoba_includes');
-
-
-/****
-Option:
-If CLICKPOP is true, hovering over the link "addr" changes it to "Click to email addr". (This switch has no effect if JavaScript is off.)
-****/
-define ("CLICKPOP", false);
 
 
 /****
@@ -87,14 +90,13 @@ function emoba_readable_mail($email="", $name="(Hover)" ) {
 
 /****
 This is the RE expression for detecting email addresses. (The result found is returned as email=>result.)
-
 ****/
 define( "ADDR_PATTERN",
         "(?P<email>[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})" );
 
 
 /****
-This encodes the email address. It converts string to HTML-hex representation of character ordinal
+This converts the email's string of character ordinals to %-hex representation
 ****/
 function emoba_hexify_mailto($mailto) {
   $hexified = '';
@@ -110,28 +112,30 @@ The JavaScript for creating the email link and popup
 ****/
 function emoba_addJScript($email, $ename, $id) {
   $link   = emoba_hexify_mailto($email);
-  $emoba_js = "
-<script type=\"text/javascript\">
+  $emoba_js = <<<AJS
+<script type="text/javascript">
   var mailtostring = 'mailto:';
   var mailNode = document.getElementById('$id');
   var linkNode = document.createElement('a');
   linkNode.title = 'Send email';
   linkNode.id = '$id';
   var mailtolink = mailtostring + '$link';
-  linkNode.href = mailtolink;";
+  linkNode.href = mailtolink;
+AJS;
 if (true == CLICKPOP) {
-  $emoba_js .= "
+  $emoba_js .= <<<AJS
   var spanNode = document.createElement('span');
   spanNode.innerHTML = 'Click to email ';
   linkNode.appendChild(spanNode);
-  linkNode.className = 'emoba-pop';";
+  linkNode.className = 'emoba-pop';
+AJS;
 }
-$emoba_js .= "
+$emoba_js .= <<<AJS
   var tNode = document.createTextNode('$ename');
   linkNode.appendChild(tNode);
   mailNode.parentNode.replaceChild(linkNode, mailNode);
 </script>
-";
+AJS;
   return $emoba_js;
 }
 
@@ -146,57 +150,62 @@ The main function.
 3. The JavaScript (via emoba_addJScript()) replaces the <span> with the approriate <a> link.(with the address encoded).
 4. CSS (for class emoba-pop) creates the hover effect when JavaScript is turned off.
 ****/
+
 function emoba_replace($content) {
-// First, convert full  <a href="mailto:A@B.C >Name</a>  links
-  $addr_pattern = '|<a href="mailto:' .ADDR_PATTERN. '"[^>]*>(?P<name>[^<]+)</a>|i';
-  preg_match_all($addr_pattern, $content, $matches, PREG_SET_ORDER);
-  foreach ( $matches as $match ) {
-    $em_email = $match[email];
-    $em_name = $match[name];
-    $id = "emoba-" . rand(1000, 9999);
-    $repaddr = "<span id=\"$id\">";
-    $repaddr .= emoba_readable_mail($em_email, $em_name) . "</span>\n";
-    $repaddr .= emoba_addJScript($em_email, $em_name, $id);
-    $repaddrs[] = $repaddr;
-    $targets[] = $match[0];
-  }
-  $content = str_replace($targets, $repaddrs, $content);
 
-// Set search pattern to A@B.C
-  $addr_pattern = ADDR_PATTERN;
+// (1) convert full  <a href="mailto:A@B.C >Name</a>  links
 
-// Remove mailto:'s from any remaining  mailto:A@B.C
-//  (this won't affect the full links just processed, since they no longer contain the string linkto:A@B.C)
-  $content = preg_replace("/mailto:".$addr_pattern."/i", '$1', $content);
-// and convert the special pattern [Name] A@B.C to email link <a href="mailto:A@B.C >Name</a>
-  preg_match_all("/\[(?P<name>[^]]+)]\s*".$addr_pattern."/i", $content, $matches, PREG_SET_ORDER);
-  foreach ( $matches as $match ) {
-    $em_email = $match[email];
-    $em_name = $match[name];
-    $id = "emoba-" . rand(1000, 9999);
-    $repaddr = "<span id=\"$id\">";
-    $repaddr .= emoba_readable_mail($em_email, $em_name). "</span>\n";
-    $repaddr .= emoba_addJScript($em_email, $em_name, $id);
-    $repaddrs[] = $repaddr;
-    $targets[] = $match[0];
-  }
-  $content = str_replace($targets, $repaddrs, $content);
+  $content = preg_replace_callback(
+    '|<a href="mailto:' .ADDR_PATTERN. '"[^>]*>(?P<name>[^<]+)</a>|i',
+    create_function(
+      '$match',
+      '$em_email = $match[email];
+			$em_name = $match[name];
+			$id = "emoba-" . rand(1000, 9999);
+			$repaddr = "<span id=\"$id\">";
+			$repaddr .= emoba_readable_mail($em_email, $em_name) . "</span>\n";
+			$repaddr .= emoba_addJScript($em_email, $em_name, $id);
+			$repaddrs[] = $repaddr;
+			return $repaddr;' ),
+    $content );
 
-// Finally, convert any remaining addresses A@B.C to link <a href="mailto:A@B.C">A-B-C</a>
-  preg_match_all('/'.$addr_pattern.'/i', $content, $matches, PREG_SET_ORDER); $j=1;
-  foreach ( $matches as $match ) {
-    $em_email = $match[email];
-    $em_name = emoba_dash_email($em_email);
-    $readable_email = emoba_glyph_email($em_email);
-    $id = "emoba-" . rand(1000, 9999);
-    $repaddr = "<span id=\"$id\">";
-    $repaddr .= $readable_email . "</span>\n";
-    $repaddr .= emoba_addJScript($em_email, $em_name, $id);
-    $repaddrs[] = $repaddr;
-    $targets[] = $match[0];
-  }
-  $content = str_replace($targets, $repaddrs, $content);
+//  We can now remove mailto:'s from any remaining  mailto:A@B.C
+//  (This won't affect the full links just processed, since they no longer contain the string linkto:A@B.C)
 
+  $content = preg_replace("|mailto:".ADDR_PATTERN."|i", '$1', $content);
+
+// (2) Convert the special pattern [Name] A@B.C to email link <a href="mailto:A@B.C >Name</a>
+
+  $content = preg_replace_callback(
+    "/\[(?P<name>[^]]+)]([\s]|&nbsp;)*".ADDR_PATTERN."/i",
+    create_function(
+      '$match',
+			'$em_email = $match[email];
+			$em_name = $match[name];
+			$id = "emoba-" . rand(1000, 9999);
+			$repaddr = "<span id=\"$id\">";
+			$repaddr .= emoba_readable_mail($em_email, $em_name). "</span>\n";
+			$repaddr .= emoba_addJScript($em_email, $em_name, $id);
+			return $repaddr;' ),
+    $content );
+
+// (3) Convert any remaining addresses A@B.C to the link <a href="mailto:A@B.C">A-B-C</a>
+
+  $content = preg_replace_callback(
+    '|'.ADDR_PATTERN.'|i',
+    create_function(
+      '$match',
+			'$em_email = $match[email];
+			$em_name = emoba_dash_email($em_email);
+			$readable_email = emoba_glyph_email($em_email);
+			$id = "emoba-" . rand(1000, 9999);
+			$repaddr = "<span id=\"$id\">";
+			$repaddr .= $readable_email . "</span>\n";
+			$repaddr .= emoba_addJScript($em_email, $em_name, $id);
+			return $repaddr;' ),
+    $content );
+
+// We're through!
   return $content;
 }
 
@@ -207,7 +216,9 @@ Finally, link emoba_replace() into WordPress filters
 add_filter('the_content', 'emoba_replace');
 add_filter('the_excerpt', 'emoba_replace');
 add_filter('comment_text', 'emoba_replace', 1); // must get there before the comment text filters do
+add_filter('widget_text', 'emoba_replace');
 add_filter('author_email', 'emoba_replace');
 add_filter('comment_email', 'emoba_replace');
+
 
 ?>
